5d3115457ffaf481a3a8a773f1e54cb900f2b682
Chris Cunningham <chris.cunningham@gogiigames.com>
2018-07-24 14:35:24 -0300

    arg changes

--- cmd/unityUtils/main.go
+++ cmd/unityUtils/main.go
@@ -11,20 +11,23 @@ import (
 )
 
 func main() {
-    if len(os.Args[1:]) > 1 {
+    if len(os.Args[1:]) == 1 {
+        runUnity(os.Args[1])
+        return
+    }
+
+    if len(os.Args[1:]) == 2 {
         switch os.Args[1] {
         case "run":
             runUnity(os.Args[2])
         case "version":
             printVersion(os.Args[2])
-        case "install":
-            install(os.Args[2])
         default:
-            fmt.Printf("%q is not a valid command.\n", os.Args[1])
+            log.Fatalf("%q is not a valid command.\n", os.Args[1])
         }
-    } else {
-        runUnity(os.Args[1])
+        return
     }
+    log.Fatalf("Received a invalid number of arguemants got %d expected 1 to 2\n", len(os.Args[1:]))
 }
 
 func runUnity(path string) {
@@ -40,15 +43,17 @@ func runUnity(path string) {
 
     appPath, err := unity.GetExecutableFromVersion(version)
     if err != nil {
-        log.Fatal(err)
+        fmt.Printf("Unity version %q not found\nDo you want to download and install? (yes|no): ", version)
+        buf := bufio.NewReader(os.Stdin)
+        data, _ := buf.ReadString('\n')
+        fmt.Println(data)
+        return
     }
 
     absProjectPath, _ := filepath.Abs(path)
-
     fmt.Printf("Opening Unity Version: %s\n", version)
 
     app := exec.Command("open", "-a", appPath, "--args", "-projectPath", absProjectPath)
-
     err = app.Run()
     if err != nil {
         log.Fatal(err)
@@ -68,15 +73,4 @@ func printVersion(path string) {
 
     _, err = unity.GetExecutableFromVersion(version)
     fmt.Printf("version: %s, installed: %t\n", version, err == nil)
-}
-
-func install(version string) {
-    buf := bufio.NewReader(os.Stdin)
-    fmt.Printf("> ")
-    data, err := buf.ReadString('\n')
-    if err != nil {
-        fmt.Println(data)
-    } else {
-        fmt.Println(data)
-    }
 }
