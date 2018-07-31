c231e1d5f81bf3a25e08576d2988d58045909110
Chris Cunningham <chris.cunningham@gogiigames.com>
2018-07-31 17:11:44 -0300

    new commands layout

--- cmd/unity-loader/main.go
+++ cmd/unity-loader/main.go
@@ -7,26 +7,24 @@ import (
 )
 
 func main() {
-    commands := unity.GetCommands()
-
     if len(os.Args) == 1 {
-        printHelp(&commands)
+        printHelp(&unity.Commands)
         return
     }
 
     if len(os.Args) == 2 {
         if _, err := os.Stat(os.Args[1]); err == nil {
-            commands["run"].Action(os.Args[1])
+            unity.Commands["run"].Action(os.Args[1])
             return
         }
     }
 
-    if val, ok := commands[os.Args[1]]; ok {
+    if val, ok := unity.Commands[os.Args[1]]; ok {
         val.Action(os.Args[2:]...)
     } else {
         fmt.Printf("%q is not a valid command\n", os.Args[1])
         fmt.Println()
-        printHelp(&commands)
+        printHelp(&unity.Commands)
     }
 }
 
--- pkg/unity/commands.go
+++ pkg/unity/commands.go
@@ -10,72 +10,75 @@ import (
 type Command struct {
     Name string
     HelpText string
-    Action func(args ...string)
+    Action CommandAction
 }
 
-func NewCommand(name string, helpText string, action func(args ...string)) *Command {
+type CommandAction func(...string) error
+
+func NewCommand(name string, helpText string, action CommandAction) *Command {
     return &Command{Name:name, HelpText:helpText, Action:action}
 }
 
-func GetCommands() map[string]*Command {
-    commands := map[string]*Command {
+var Commands = map[string]*Command {
 
-        "run": NewCommand(
-            "run",
-            "run the passed in project with a auto detected version of unity",
-            func(args ...string) {
-                path := args[0]
+    "run": NewCommand(
+        "run",
+        "run the passed in project with a auto detected version of unity",
+        func(args ...string) error {
+            path := args[0]
 
-                versionFile := filepath.Join(path, "ProjectSettings", "ProjectVersion.txt")
-                if _, err := os.Stat(versionFile); os.IsNotExist(err) {
-                    fmt.Printf("%q is not a valid unity project\n", path)
-                }
+            versionFile := filepath.Join(path, "ProjectSettings", "ProjectVersion.txt")
+            if _, err := os.Stat(versionFile); os.IsNotExist(err) {
+                fmt.Printf("%q is not a valid unity project\n", path)
+            }
 
-                version, err := GetVersionFromProject(versionFile)
-                if err != nil {
-                    log.Fatal(err)
-                }
+            version, err := GetVersionFromProject(versionFile)
+            if err != nil {
+                log.Fatal(err)
+            }
 
-                appInstall, err := GetInstallFromVersion(version)
-                if err != nil {
-                    log.Fatalf("Unity version %q not found", version)
-                }
+            appInstall, err := GetInstallFromVersion(version)
+            if err != nil {
+                log.Fatalf("Unity version %q not found", version)
+            }
 
-                fmt.Printf("Opening project %q in version: %s\n", path, version)
-                err = appInstall.Run(path)
-                if err != nil {
-                    log.Fatalf("Could not execute unity from %q", appInstall.Path)
-                }
-            }),
+            fmt.Printf("Opening project %q in version: %s\n", path, version)
+            err = appInstall.Run(path)
+            if err != nil {
+                log.Fatalf("Could not execute unity from %q", appInstall.Path)
+            }
+            return nil
+        }),
 
-        "version": NewCommand(
-            "version",
-            "check what version of unity a project is using",
-            func(args ...string) {
-                path := args[0]
+    "version": NewCommand(
+        "version",
+        "check what version of unity a project is using",
+        func(args ...string) error {
+            path := args[0]
 
-                versionFile := filepath.Join(path, "ProjectSettings", "ProjectVersion.txt")
-                if _, err := os.Stat(versionFile); os.IsNotExist(err) {
-                    fmt.Printf("%q is not a valid unity project\n", path)
-                }
+            versionFile := filepath.Join(path, "ProjectSettings", "ProjectVersion.txt")
+            if _, err := os.Stat(versionFile); os.IsNotExist(err) {
+                fmt.Printf("%q is not a valid unity project\n", path)
+            }
 
-                version, err := GetVersionFromProject(versionFile)
-                if err != nil {
-                    log.Fatal(err)
-                }
+            version, err := GetVersionFromProject(versionFile)
+            if err != nil {
+                log.Fatal(err)
+            }
 
-                _, err = GetInstallFromVersion(version)
-                fmt.Printf("version: %s, installed: %t\n", version, err == nil)
-            }),
+            _, err = GetInstallFromVersion(version)
+            fmt.Printf("version: %s, installed: %t\n", version, err == nil)
 
-        "list": NewCommand(
-            "list",
-            "list all installed unity versions",
-            func(args ...string) {
-                for _, data := range GetInstalls() {
-                    fmt.Printf("Version: %s Path: %q", data.Version, data.Path)
-                }
-            }),
-    }
-    return commands
+            return nil
+        }),
+
+    "list": NewCommand(
+        "list",
+        "list all installed unity versions",
+        func(args ...string) error {
+            for _, data := range GetInstalls() {
+                fmt.Printf("Version: %s Path: %q", data.Version, data.Path)
+            }
+            return nil
+        }),
 }
