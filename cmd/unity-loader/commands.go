9bf11946b677a65d0d00940e79fcba1d350b0b8b
Chris Cunningham <chris.cunningham@gogiigames.com>
2018-08-24 13:33:17 -0300

    fixed merge issue

--- cmd/unity-loader/commands.go
+++ cmd/unity-loader/commands.go
@@ -6,21 +6,31 @@ import (
     "os"
     "fmt"
     "log"
+    "errors"
+    "gopkg.in/AlecAivazis/survey.v1"
 )
 
-type Command struct {
+type command struct {
     Name string
     HelpText string
     Action func(...string) error
 }
 
-var Commands = map[string]Command {
+var commandOrder = [...]string{"run", "version", "list", "install", "repair"}
+
+var commands = map[string]command {
 
     "run": {
         "run",
-        "run the passed in project with a auto detected version of unity",
+        "run the passed in project with an auto detected version of unity",
         func(args ...string) error {
-            path := args[0]
+            var path string
+
+            if len(args) == 0 {
+                path, _ = os.Getwd()
+            } else {
+                path = args[0]
+            }
 
             versionFile := filepath.Join(path, "ProjectSettings", "ProjectVersion.txt")
             if _, err := os.Stat(versionFile); os.IsNotExist(err) {
@@ -29,18 +39,30 @@ var Commands = map[string]Command {
 
             version, err := unity.GetVersionFromProject(versionFile)
             if err != nil {
-                log.Fatal(err)
+                return err
             }
 
             appInstall, err := unity.GetInstallFromVersion(version)
             if err != nil {
-                log.Fatalf("Unity version %q not found", version)
+                if _, ok := err.(unity.VersionNotFoundError); ok {
+                    fmt.Printf("Unity %s not installed\n", version)
+                    installUnity := false
+                    prompt := &survey.Confirm{
+                        Message: fmt.Sprintf("Do you want to install Unity %s?", version),
+                    }
+                    survey.AskOne(prompt, &installUnity, nil)
+                    if installUnity {
+                        Install(version)
+                    }
+                } else {
+                    return err
+                }
             }
 
             fmt.Printf("Opening project %q in version: %s\n", path, version)
             err = appInstall.Run(path)
             if err != nil {
-                log.Fatalf("Could not execute unity from %q", appInstall.Path)
+                return fmt.Errorf("could not execute unity from %q", appInstall.Path)
             }
             return nil
         },
@@ -50,16 +72,22 @@ var Commands = map[string]Command {
         "version",
         "check what version of unity a project is using",
         func(args ...string) error {
-            path := args[0]
+            var path string
+
+            if len(args) == 0 {
+                path, _ = os.Getwd()
+            } else {
+                path = args[0]
+            }
 
             versionFile := filepath.Join(path, "ProjectSettings", "ProjectVersion.txt")
             if _, err := os.Stat(versionFile); os.IsNotExist(err) {
-                fmt.Printf("%q is not a valid unity project\n", path)
+                return fmt.Errorf("%q is not a valid unity project\n", path)
             }
 
             version, err := unity.GetVersionFromProject(versionFile)
             if err != nil {
-                log.Fatal(err)
+                return err
             }
 
             _, err = unity.GetInstallFromVersion(version)
@@ -74,7 +102,43 @@ var Commands = map[string]Command {
         "list all installed unity versions",
         func(args ...string) error {
             for _, data := range unity.GetInstalls() {
-                fmt.Printf("Version: %s Path: %q", data.Version, data.Path)
+                fmt.Printf("Version: %s Path: %q\n", data.Version, data.Path)
+            }
+            return nil
+        },
+    },
+
+    "install": {
+        "install",
+        "installed the specified version of unity",
+        func(args ...string) error {
+            if len(args) == 0 {
+                return errors.New("no version specified")
+            }
+
+            version := args[0]
+            err := Install(version)
+            if err != nil {
+                log.Fatal("ERROR: ", err)
+            }
+            return nil
+        },
+    },
+
+    "repair": {
+        "repair",
+        "fix paths to unity installs",
+        func(args ...string) error {
+            fmt.Println("repairing unity install paths")
+            for _, install := range unity.GetInstalls() {
+                oldPath := filepath.Dir(install.Path)
+                newName := fmt.Sprintf("Unity %s", install.Version)
+                newPath := filepath.Join("/Applications/", newName)
+
+                if oldPath == newPath {continue}
+                fmt.Printf("moveing %q to %q\n", oldPath, newPath)
+                err := os.Rename(oldPath, newPath)
+                if err != nil {return err}
             }
             return nil
         },
