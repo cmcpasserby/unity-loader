a0ea7ba1d21b83b5e993a4dcf3039d5b7054b937
Chris Cunningham <chris.cunningham@gogiigames.com>
2019-01-28 16:35:59 -0400

    run update run config added

--- cmd/unity-loader/commands/update.go
+++ cmd/unity-loader/commands/update.go
@@ -2,23 +2,22 @@ package commands
 
 import (
 	"github.com/cmcpasserby/unity-loader/pkg/parsing"
-	"github.com/cmcpasserby/unity-loader/pkg/settings"
 )
 
 func update(args ...string) error {
-	// err := parsing.GetArchiveVersions()
-	// if err != nil {
-	// 	return err
-	// }
-
-	data, err := parsing.GetHubVersions()
+	err := parsing.GetArchiveVersions()
 	if err != nil {
 		return err
 	}
 
-	if err := settings.WriteCache(data); err != nil {
-		return err
-	}
+	// data, err := parsing.GetHubVersions()
+	// if err != nil {
+	// 	return err
+	// }
+	//
+	// if err := settings.WriteCache(data); err != nil {
+	// 	return err
+	// }
 
 	return nil
 }
