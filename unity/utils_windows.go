eb09be0544234021ed4759b489f7b370df5c2c26
Chris Cunningham <chris@rightsomegoodgames.ca>
2021-10-21 15:45:27 -0300

    stubbing out windows functions

---
+++ unity/utils_windows.go
@@ -0,0 +1,17 @@
+package unity
+
+func (info *InstallInfo) RunWithTarget(project, target string) error {
+	panic("not implemented yet")
+}
+
+func (info *InstallInfo) NewProject(projectName string) error {
+	panic("not implemented yet")
+}
+
+func GetInstalls() ([]*InstallInfo, error) {
+	panic("not implemented yet")
+}
+
+func GetInstallFromPath(path string) (*InstallInfo, error) {
+	panic("not implemented yet")
+}
