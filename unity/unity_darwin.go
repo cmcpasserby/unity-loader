8691c890c882514fe79b687ace859c07d62e5571
Chris Cunningham <chris@rightsomegoodgames.ca>
2022-04-25 08:58:56 -0300

    refactoring of the macOS app parsing

--- unity/unity_darwin.go
+++ unity/unity_darwin.go
@@ -12,16 +12,25 @@ type appInfoDict struct {
 	CFBundleVersion    string `plist:"CFBundleVersion"`
 }
 
-func getFromInstallPathInternal(path string) (InstallInfo, error) {
+func unmarshalAppInfo(path string) (appInfoDict, error) {
 	plistPath := filepath.Join(path, "Contents", "Info.plist")
-	file, err := os.Open(plistPath)
+	f, err := os.Open(plistPath)
 	if err != nil {
-		return InstallInfo{}, err
+		return appInfoDict{}, err
 	}
-	defer closeFile(file)
+	defer closeFile(f)
 
 	var appInfo appInfoDict
-	if err = plist.NewDecoder(file).Decode(&appInfo); err != nil {
+	if err = plist.NewDecoder(f).Decode(&appInfo); err != nil {
+		return appInfoDict{}, err
+	}
+
+	return appInfo, nil
+}
+
+func getFromInstallPathInternal(path string) (InstallInfo, error) {
+	appInfo, err := unmarshalAppInfo(path)
+	if err != nil {
 		return InstallInfo{}, err
 	}
 
@@ -30,8 +39,16 @@ func getFromInstallPathInternal(path string) (InstallInfo, error) {
 		return InstallInfo{}, err
 	}
 
-	installData := InstallInfo{Path: path, Version: ver}
-	return installData, nil
+	return InstallInfo{Path: path, Version: ver}, nil
+}
+
+func binFromApp(path string) (string, error) {
+	appInfo, err := unmarshalAppInfo(path)
+	if err != nil {
+		return "", err
+	}
+
+	return filepath.Join(path, "Contents", "MacOS", appInfo.CFBundleExecutable), nil
 }
 
 func unityGlob(searchPath string) ([]string, error) {
@@ -41,20 +58,4 @@ func unityGlob(searchPath string) ([]string, error) {
 func command(path string, args ...string) *exec.Cmd {
 	newArgs := append([]string{path, "-W", "-n", "--args"}, args...)
 	return exec.Command("open", newArgs...)
-}
-
-func binFromApp(path string) (string, error) {
-	plistPath := filepath.Join(path, "Contents", "Info.plist")
-	file, err := os.Open(plistPath)
-	if err != nil {
-		return "", err
-	}
-	defer closeFile(file)
-
-	var appInfo appInfoDict
-	if err = plist.NewDecoder(file).Decode(&appInfo); err != nil {
-		return "", err
-	}
-
-	return filepath.Join(path, "Contents", "MacOS", appInfo.CFBundleExecutable), nil
 }
